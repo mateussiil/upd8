931c2c01b805af71ebece89b591e951ab062fa23
Mateus Silva <mateus_silva97@hotmail.com>
2023-06-12 15:04:33 -0300

    feat: add user model & update user table

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,12 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'cpf',
+        'birthdate',
+        'gender',
+        'address',
+        'state',
+        'city',
     ];
 
     /**
---
+++ database/migrations/2023_06_12_175841_add_columns_to_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('cpf')->unique();
+            $table->date('birthdate');
+            $table->string('gender');
+            $table->string('address');
+            $table->string('state');
+            $table->string('city');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['cpf', 'birthdate', 'gender', 'address', 'state', 'city']);
+        });
+    }
+};
